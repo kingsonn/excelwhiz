17f3a9513c44b8535011c1bfbf85f949a9bad43c
kingsonn <hansonnnnnnn@gmail.com>
2023-05-29 01:06:21 +0530

    contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,12 +39,12 @@ export const Contact = (props) => {
             <div className="row">
               <div className="section-title">
                 <h2>Get In Touch</h2>
-                <p>
+                {/* <p>
                   Please fill out the form below to send us an email and we will
                   get back to you as soon as possible.
-                </p>
+                </p> */}
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
+              {/* <form name="sentMessage" validate onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-md-6">
                     <div className="form-group">
@@ -91,7 +91,7 @@ export const Contact = (props) => {
                 <button type="submit" className="btn btn-custom btn-lg">
                   Send Message
                 </button>
-              </form>
+              </form> */}
             </div>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
@@ -104,14 +104,14 @@ export const Contact = (props) => {
                 {props.data ? props.data.address : "loading"}
               </p>
             </div> */}
-            <div className="contact-item">
+            {/* <div className="contact-item">
               <p>
                 <span>
                   <i className="fa fa-phone"></i> Phone
                 </span>{" "}
                 {props.data ? props.data.phone : "loading"}
               </p>
-            </div>
+            </div> */}
             <div className="contact-item">
               <p>
                 <span>
